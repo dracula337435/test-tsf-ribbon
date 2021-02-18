ecb3c56fff3cf6ff8e47f3a196e2bc12bde72784
dk <dracula337435@163.com>
2021-02-18 16:29:27 +0800

    增加RestTemplate的使用示例

--- client/src/main/java/io/dracula/test/tsf/ribbon/client/TestController.java
+++ client/src/main/java/io/dracula/test/tsf/ribbon/client/TestController.java
@@ -3,6 +3,7 @@ package io.dracula.test.tsf.ribbon.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author dk
@@ -13,9 +14,17 @@ public class TestController {
     @Autowired
     private ToServerService toServerService;
 
-    @GetMapping("server")
-    public String toServer(){
+    @GetMapping("feign")
+    public String feign(){
         return toServerService.sayHello();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("rest")
+    public String rest(){
+        return restTemplate.getForObject("http://test-server/hello", String.class);
     }
 
 }
--- client/src/main/java/io/dracula/test/tsf/ribbon/client/TestTsfRibbonClientMainApp.java
+++ client/src/main/java/io/dracula/test/tsf/ribbon/client/TestTsfRibbonClientMainApp.java
@@ -2,8 +2,11 @@ package io.dracula.test.tsf.ribbon.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.tsf.annotation.EnableTsf;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author dk
@@ -15,6 +18,12 @@ public class TestTsfRibbonClientMainApp {
 
     public static void main(String[] args){
         SpringApplication.run(TestTsfRibbonClientMainApp.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
